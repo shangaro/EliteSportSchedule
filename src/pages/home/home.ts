8f4818e2f8191cbe6afc1d305c1325f91611becd
Dibas Baral <nicolauscafe@hotmail.com>
2017-11-16 15:14:33 -0500

    added favourites in home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,6 +7,18 @@ import { TournamentsPage } from '../tournaments/tournaments';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  favourites=[
+    {
+      team:{ id:6182 , name: 'Manchester United', coach:'Giovani'},
+      tournamentId:'12345678',
+      tournamentName:'Barclays Premier League'
+    },
+    {
+      team:{ id:6184 , name: 'Liver Pool', coach:'Gerrard'},
+      tournamentId:'123456789',
+      tournamentName:'English Premiere League'
+    }
+  ];
 
   constructor(public navCtrl: NavController) {
 
